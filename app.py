01fd68c2b6d657dfc996c468b3c9d441b13bfea6
Avik Das <avikdas567@gmail.com>
2025-06-09 21:37:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from sklearn.feature_extraction.text import CountVectorizer
 
-texts = ["i love data science, i love data", "Data science is future"]
+texts = ["I love data science, I love data", "Data science is future"]
 
-vectorizer = CountVectorizer()
+vectorizer = CountVectorizer(tokenizer=str.split)
 X = vectorizer.fit_transform(texts)
 
 st.write("Vocabulary:", vectorizer.get_feature_names_out())
