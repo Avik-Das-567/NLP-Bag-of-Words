bf8000d1f40ba72fbc8645d7d97ef65de980a31b
Avik Das <avikdas567@gmail.com>
2025-06-09 21:23:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from sklearn.feature_extraction.text import CountVectorizer
+
+texts = ["I love data science, I love data", "Data science is future"]
+
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(texts)
+
+st.write("Vocabulary:", vectorizer.get_feature_names_out())
+st.write("BoW Matrix:\n", X.toarray())
