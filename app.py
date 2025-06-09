eaa988cd801112537aebdc0e7aa2dafa79cf2177
Avik Das <avikdas567@gmail.com>
2025-06-09 21:44:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 texts = ["I love data science, I love data", "Data science is future"]
 
-vectorizer = CountVectorizer(tokenizer=str.split)
+vectorizer = CountVectorizer(token_pattern=r'(?u)\b\w+\b')
 X = vectorizer.fit_transform(texts)
 
 st.write("Vocabulary:", vectorizer.get_feature_names_out())
