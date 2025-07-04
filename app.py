d204006af2472c1fb9778ee7fa667a92cd87bcae
Avik Das <avikdas567@gmail.com>
2025-07-04 14:40:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ texts = ["I love data science, I love data", "Data science is future"]
 vectorizer = CountVectorizer(token_pattern=r'(?u)\b\w+\b')
 X = vectorizer.fit_transform(texts)
 
+st.write("Input Texts :-", texts)
 st.write("Vocabulary:", vectorizer.get_feature_names_out())
 st.write("BoW Matrix:\n", X.toarray())
